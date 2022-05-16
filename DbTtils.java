1e25919c3c8167de0c0dd42c54d6ecf252f90c87
Ankit9955 <62520038+Ankit9955@users.noreply.github.com>
2022-05-16 22:51:44 +0530

    Add files via upload

---
+++ DbTtils.java
@@ -0,0 +1,21 @@
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranu kumar
+ */
+class DbTtils {
+
+    static TableModel resultSetToTableMode1(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
